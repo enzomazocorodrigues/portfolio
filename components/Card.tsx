f5d9067633cb0dee233a295000eeb849c2a05afc
enzomazocorodrigues <enzo.rodrigues2@gmail.com>
2022-10-21 17:58:38 -0300

    fix: align card color pallete with footer background

--- components/Card.tsx
+++ components/Card.tsx
@@ -8,7 +8,7 @@ interface CardProps {
 export function Card({ children, className }: CardProps) {
   return (
     <article
-      className={`px-3 py-4 bg-slate-700 border border-slate-500 rounded-md ${className}`}
+      className={`px-3 py-4 bg-slate-800 border border-slate-500 rounded-md ${className}`}
     >
       {children}
     </article>
