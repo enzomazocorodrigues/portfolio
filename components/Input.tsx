dd952aa433a2412949747b79f0dea2eadb888576
enzomazocorodrigues <enzo.rodrigues2@gmail.com>
2022-10-21 18:35:49 -0300

    fix: increase input text opacity on safari

--- components/Input.tsx
+++ components/Input.tsx
@@ -5,7 +5,7 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
 export function Input({ className, ...props }: InputProps) {
   return (
     <input
-      className={`bg-slate-700 w-full rounded-md outline-none px-4 py-3 border border-slate-500 text-white text-sm ${className}`}
+      className={`bg-slate-700 w-full rounded-md outline-none px-4 py-3 border border-slate-500 text-white text-sm [--webkit-opacity:1] [-webkit-text-fill-color: white] ${className}`}
       {...props}
     />
   );
